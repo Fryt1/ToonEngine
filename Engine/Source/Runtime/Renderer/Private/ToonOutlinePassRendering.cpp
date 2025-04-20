ba82899b5b9e8bc4ef76cfc5dc90703a433397cb
Fry <2764896313@qq.com>
2025-04-20 15:11:44 +0800

    first commit

---
+++ Engine/Source/Runtime/Renderer/Private/ToonOutlinePassRendering.cpp
@@ -0,0 +1,73 @@
+﻿// ToonOutlinePassRendering.cpp
+#include "ToonOutlinePassRendering.h"  
+
+#include "DeferredShadingRenderer.h"  
+#include "ScenePrivate.h"  
+#include "MeshPassProcessor.inl"  
+#include "SimpleMeshDrawCommandPass.h"  
+#include "ProfilingDebugging/CsvProfiler.h"
+
+FToonOutlineMeshPassProcessor::FToonOutlineMeshPassProcessor(  
+	const FScene* Scene,   
+	const FSceneView* InViewIfDynamicMeshCommand,   
+	const FMeshPassProcessorRenderState& InPassDrawRenderState,   
+	FMeshPassDrawListContext* InDrawListContext)  
+	:FMeshPassProcessor(EMeshPass::ToonOutlinePass, Scene, Scene->GetFeatureLevel(), InViewIfDynamicMeshCommand, InDrawListContext),  
+	PassDrawRenderState(InPassDrawRenderState)  
+{  
+	if (PassDrawRenderState.GetDepthStencilState() == nullptr)  
+	{       
+		PassDrawRenderState.SetDepthStencilState(TStaticDepthStencilState<false, CF_DepthNearOrEqual>().GetRHI());  
+	}    
+	if (PassDrawRenderState.GetBlendState() == nullptr)  
+	{       
+		PassDrawRenderState.SetBlendState(TStaticBlendState<>().GetRHI());  
+	}
+}
+
+// ToonOutlinePassRendering.cpp
+void FToonOutlineMeshPassProcessor::AddMeshBatch(  
+	const FMeshBatch& MeshBatch,  
+	uint64 BatchElementMask,  
+	const FPrimitiveSceneProxy* PrimitiveSceneProxy,  
+	int32 StaticMeshId)  
+{  
+	const FMaterialRenderProxy* MaterialRenderProxy = MeshBatch.MaterialRenderProxy;  
+	const FMaterial* Material = MaterialRenderProxy->GetMaterialNoFallback(FeatureLevel);  
+
+	if (Material != nullptr && Material->GetRenderingThreadShaderMap())  
+	{       
+		const FMaterialShadingModelField ShadingModels = Material->GetShadingModels();  
+		// Only Toon shading model and enable render toon outline can render this pass  
+		if (ShadingModels.HasShadingModel(MSM_Toon) && Material->ShouldRenderToonOutline())  
+		{          
+			const EBlendMode BlendMode = Material->GetBlendMode();  
+			bool bResult = true;  
+			if (BlendMode == BLEND_Opaque)  
+			{             
+				Process(  
+					MeshBatch,                
+					BatchElementMask,                
+					StaticMeshId,                
+					PrimitiveSceneProxy,                
+					*MaterialRenderProxy,                
+					*Material,                
+					FM_Solid,  
+					CM_CCW); // Cull back  
+			}  
+		}    
+	}
+}
+// ToonOutlinePassRendering.cpp
+bool FToonOutlineMeshPassProcessor::Process(  
+    const FMeshBatch& MeshBatch,  
+    uint64 BatchElementMask,  
+    int32 StaticMeshId,  
+    const FPrimitiveSceneProxy* PrimitiveSceneProxy,  
+    const FMaterialRenderProxy& MaterialRenderProxy,  
+    const FMaterial& MaterialResource,  
+    ERasterizerFillMode MeshFillMode,  
+    ERasterizerCullMode MeshCullMode)  
+{  
+    return false;
+}
---
+++ Engine/Source/Runtime/Renderer/Private/ToonOutlinePassRendering.h
@@ -0,0 +1,39 @@
+﻿// ToonOutlinePassRendering.h
+#pragma once  
+
+#include "DataDrivenShaderPlatformInfo.h"  
+#include "MeshPassProcessor.h"  
+#include "CoreMinimal.h"  
+#include "MeshMaterialShader.h"  
+
+class FToonOutlineMeshPassProcessor : public FMeshPassProcessor  
+{  
+public:  
+	//const FScene* InScene, ERHIFeatureLevel::Type InFeatureLevel, const FSceneView* InViewIfDynamicMeshCommand, FMeshPassDrawListContext* InDrawListContext  
+	FToonOutlineMeshPassProcessor(  
+	   const FScene* Scene,  
+	   const FSceneView* InViewIfDynamicMeshCommand,  
+	   const FMeshPassProcessorRenderState& InPassDrawRenderState,  
+	   FMeshPassDrawListContext* InDrawListContext  
+	);  
+	virtual void AddMeshBatch(  
+	   const FMeshBatch& RESTRICT MeshBatch,  
+	   uint64 BatchElementMask,  
+	   const FPrimitiveSceneProxy* RESTRICT PrimitiveSceneProxy,  
+	   int32 StaticMeshId = -1  
+	) override final;  
+
+private:  
+	bool Process(  
+	   const FMeshBatch& MeshBatch,  
+	   uint64 BatchElementMask,  
+	   int32 StaticMeshId,  
+	   const FPrimitiveSceneProxy* RESTRICT PrimitiveSceneProxy,  
+	   const FMaterialRenderProxy& RESTRICT MaterialRenderProxy,  
+	   const FMaterial& RESTRICT MaterialResource,  
+	   ERasterizerFillMode MeshFillMode,  
+	   ERasterizerCullMode MeshCullMode  
+	);  
+private:  
+	FMeshPassProcessorRenderState PassDrawRenderState;  
+};
